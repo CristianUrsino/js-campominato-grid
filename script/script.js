572115ce23fb095cef8edc7b41e14dd9f1804d67
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-10-18 15:27:13 +0200

    added style

---
+++ script/script.js
@@ -0,0 +1,7 @@
+/*
+al click del btn:
+    la griglia viene creata (10*10)
+    al cui interno ci sono 100 BOX numerati
+    al click sul box:
+        si colora di azzurro e appare l'output del numero
+*/
